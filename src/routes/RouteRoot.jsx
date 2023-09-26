afa9992dfe1ebe4404504c4460b1b511ca14807d
trainerfrontend <praveen.trainer.@outlook.com>
2023-09-26 12:30:45 +0100

    nested layout

--- src/routes/RouteRoot.jsx
+++ src/routes/RouteRoot.jsx
@@ -8,9 +8,12 @@ import About from '../Pages/About';
 import TechStack from "../Pages/TechStack";
 import Contact from "../Pages/Contact";
 import RootLayout from "../Layout/RootLayout";
+import ContactLayout from "../Layout/ContactLayout";
 import App from "../App";
 import Error from '../components/common/Error';
 import Detail from '../Pages/Detail';
+import Faq from "../Pages/Faq";
+import Help from "../Pages/Help";
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
@@ -18,7 +21,12 @@ const Router = createBrowserRouter(
             <Route index element={<App />} />
             <Route path="about" element={<About />} />
             <Route path="techstack" element={<TechStack />} />
-            <Route path="contact" element={<Contact />} />
+
+            <Route path="contact" element={<ContactLayout />}>
+                <Route path="faq" element={<Faq />} />
+                <Route path="help" element={<Help />} />
+            </Route>
+
             <Route path="techstack/:userid" element={<Detail />} />
             <Route path="*" element={<Error />} />
         </Route>)
