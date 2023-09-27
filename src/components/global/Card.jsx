e4bb6f17efe2409d7882b746a7c65a8784060ade
trainerfrontend <praveen.trainer.@outlook.com>
2023-09-27 10:29:11 +0100

    custom hooks

--- src/components/global/Card.jsx
+++ src/components/global/Card.jsx
@@ -2,8 +2,15 @@ import UseFetch from "../../hooks/UseFetch";
 import CardItem from "./CardItem";
 
 const Card = () => {
-    const { data } = UseFetch("https://jsonplaceholder.typicode.com/users");
+    const { data, isLoading, isError } = UseFetch("https://jsonplaceholder.typicode.com/users");
 
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (isError) {
+        return <h1>Error...</h1>
+    }
     return <section>
         <CardItem usersData={data} />
     </section>
--- src/hooks/UseFetch.jsx
+++ src/hooks/UseFetch.jsx
@@ -2,17 +2,20 @@ import { useEffect, useState } from "react";
 
 const useFetch = (URL) => {
     const [data, setData] = useState();
-    const [isLoading, setIsLoading] = useState();
+    const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState();
 
     useEffect(() => {
         fetch(URL)
             .then(response => response.json())
-            .then(data => setData(data))
+            .then(data => {
+                setData(data);
+                setIsLoading(false)
+            })
             .catch((error) => setIsError(error));
     }, [])
 
-    return { data, isError, }
+    return { data, isError, isLoading }
 }
 
 export default useFetch;
